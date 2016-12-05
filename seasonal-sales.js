24f99c6967160d9a65b642660c1acd3bbc8324d2
Jufebrown <jufebrown@gmail.com>
2016-12-05 14:05:58 -0600

    added a function to get the discount

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -6,6 +6,7 @@ var catData;
 var prodLoaded;
 var catLoaded;
 
+
 prodRequest.addEventListener('load', prodLoad);
 prodRequest.open('GET', 'products.json');
 prodRequest.send();
@@ -13,6 +14,18 @@ prodRequest.send();
 catRequest.addEventListener('load', catLoad);
 catRequest.open('GET', 'categories.json');
 catRequest.send();
+
+function getDiscount(departmentId) {
+  var seasonSelected = document.querySelector(".season").value;
+  var catSeason = catData.categories[departmentId].season_discount;
+  var discount = catData.categories[departmentId].discount;
+  if (seasonSelected === catSeason) {
+    return discount;
+  } else {
+    discount = 0;
+    return discount;
+  }
+}
 
 function printProducts() {
   for (var i = 0; i < prodData.products.length; i++) {
@@ -23,6 +36,8 @@ function printProducts() {
     console.log(departmentId, typeof(departmentId))
     var department = catData.categories[departmentId].name;
     console.log(department);
+    getDiscount(departmentId);
+
     // prodTable += `<trow><td>${prodName}</td><td>${department}</td><td>${price}</td>`
   }
 }
